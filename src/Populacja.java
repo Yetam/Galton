8e1cd62a299dc34ef17fcb6aa8afabaad748d7a8
dominik setniewski <setniewskidominik@gmail.com>
2019-01-13 20:27:18 +0100

    x

--- src/Populacja.java
+++ src/Populacja.java
@@ -1,32 +1,42 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Populacja {
 	ArrayList<Galton> pop = new ArrayList<Galton>();
 	ArrayList<Galton> kid = new ArrayList<Galton>();
-	
-	Populacja(){
-		//screen not connected...
+
+	Populacja() {
+		// screen not connected...
 	}
-	
-	void createGaltonPopulation(int n){
-		for(int i=0;i<n;i++){
+
+	void createGaltonPopulation(int n) {
+		for (int i = 0; i < n; i++) {
 			pop.add(new Galton(20));
 		}
 	}
-	
-	void simulateGaltonPopulation(){
-		
+
+	void simulateGaltonPopulation() {
+
 	}
-	void sortGaltonPopulation(){
-		
+
+	void sortGaltonPopulation() {
+		pop.sort(new Comparator<Galton>() {
+			@Override
+			public int compare(Galton o1, Galton o2) {
+				return o1.fitness > o2.fitness ? -1 : 1;
+			}
+		});
 	}
-	void createKidPopulation(){
-		
+
+	void createKidPopulation() {
+
 	}
-	void joinKidsAndPopulation(){
-		
+
+	void joinKidsAndPopulation() {
+
 	}
-	void mutatePopulation(){
-		
+
+	void mutatePopulation() {
+
 	}
 }
