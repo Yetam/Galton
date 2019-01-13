b0482dad34c687c492ec3c2ac8ce431e26714cac
Yetam <jetam@vp.pl>
2019-01-13 20:55:46 +0100

    Na etapie tworzenia createKidPopulation

--- src/Populacja.java
+++ src/Populacja.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Random;
 
 public class Populacja {
 	ArrayList<Galton> pop = new ArrayList<Galton>();
 	ArrayList<Galton> kid = new ArrayList<Galton>();
+	
+	double sumOfFitness;
+	Random rnd = new Random();
 
 	Populacja() {
 		// screen not connected...
@@ -16,7 +20,9 @@ public class Populacja {
 	}
 
 	void simulateGaltonPopulation() {
-
+		for(int i=0;i<pop.size();i++){
+			pop.get(i).simulate();
+		}
 	}
 
 	void sortGaltonPopulation() {
@@ -29,11 +35,46 @@ public class Populacja {
 	}
 
 	void createKidPopulation() {
-
+		sumOfFitness=0;
+		for(int i=0 ; i<pop.size()/2 ; i++){
+			sumOfFitness += pop.get(i).fitness;
+		}
+		
+		for(int i=0;i<pop.size()/2;i++){
+			
+			/**
+			 * Ponizsze liniki maja wylosowac dwoch roznych rodzicow z populacji
+			 */
+			int parent1 = 0;
+			int parent2 = 0;
+			while(parent1 == parent2){
+				parent1 = choseByRoulette();
+				parent2 = choseByRoulette();
+			}
+			
+		}
+		
 	}
+	int choseByRoulette(){
+		//wyznacz liczbe losowa od 0 do sumFitness
+		double roulette = rnd.nextDouble()*sumOfFitness;
+		int chosen=pop.size()-1;
+		
+		//odejmuj od wylosowanej kolejne fitnesy tak dlugo az nie bedzie mniejsza od zera. Jesli bedzie to oznacza ze dany iterator to wylosowany element
+		for(int i=0;i<pop.size();i++){
+			roulette-=pop.get(i).fitness;
+			if(roulette<0){
+				chosen = i;
+				break;
+			}
+		}
+		
+		return chosen;
+	}
+	
 
 	void joinKidsAndPopulation() {
-
+	
 	}
 
 	void mutatePopulation() {
